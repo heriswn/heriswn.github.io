8fae4038c50829791531e8a516895799296fe542
herionlinux <5sujmea6@duck.com>
2022-04-20 21:56:23 +0700

    Release v0.2.2-alpha

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,10 +1,3 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
