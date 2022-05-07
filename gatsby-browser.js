7efcc6eb286efb6fdef77a7c5b1fbb1b930fe36f
herionlinux <5sujmea6@duck.com>
2022-05-08 01:40:58 +0700

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
-// import "./src/styles/normalize.css"
+import "./src/styles/normalize.css"
 // custom CSS styles
 import "./src/styles/style.css"
 
