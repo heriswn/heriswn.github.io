84f8539028ac38932d5e5ac2b635bb62daf2a70f
herionlinux <5sujmea6@duck.com>
2022-05-16 15:47:29 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import PostListing from "../components/postlist"
+import PostListing from "../components/postList"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
