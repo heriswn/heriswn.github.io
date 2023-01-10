d35fd28547245f1397b154fbc4df5cab80e4ff74
Heri Setiawan <heri15002@mail.unpad.ac.id>
2023-01-10 08:49:59 +0700

    fixing

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ const NotFoundPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn't exist... the sadness.</p>
     </Layout>
   )
 }
